1fb8f0cb85341ed30c19a4bc2e296ec60a482d45
AndreVeelet <97946980+AndreVeelet@users.noreply.github.com>
2024-11-24 20:08:25 +0300

    Add files via upload

--- 0+/script.js
+++ 0+/script.js
@@ -1,33 +1,23 @@
 let isWindowActive = false; // Переменная для отслеживания состояния окна
 // Функция для прекращения действия по умолчанию
 document.addEventListener('touchmove', function(e) {
-    if (isWindowActive) {
-        e.preventDefault(); // Предотвращаем прокрутку, если окно активно
-    }
+    if (isWindowActive) { e.preventDefault(); } // Предотвращаем прокрутку, если окно активно
 }, { passive: false });
 
 let wakeLock = null; // Переменная для хранения блокировки экрана
 
 // Функция для запроса блокировки экрана
 async function requestWakeLock() {
-    try {
+    try { 
         wakeLock = await navigator.wakeLock.request('screen');
-
-        // Обновляем состояние каждые 5 секунд
-        setInterval(null, 5000);
-
-        // Освобождаем блокировку при закрытии вкладки
-        window.addEventListener('unload', () => {
+        setInterval(null, 5000); // Обновляем состояние каждые 5 секунд
+        window.addEventListener('unload', () => { // Освобождаем блокировку при закрытии вкладки
             if (wakeLock) { wakeLock.release(); }
         });
-
-        // Слушаем событие освобождения блокировки
-        wakeLock.addEventListener('release', () => {
+        wakeLock.addEventListener('release', () => {  // Слушаем событие освобождения блокировки
             wakeLock = null; // Сбрасываем переменную
         });
-
-    } catch (err) {
-        console.error(`${err.name}, ${err.message}`);
+    } catch (err) { console.error(`${err.name}, ${err.message}`);
     }
 }
 
@@ -35,18 +25,14 @@ async function requestWakeLock() {
 const checkbox = document.getElementById('fullscreenCheckbox');
 checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
-        // Включаем полноэкранный режим
-        document.documentElement.requestFullscreen()
-            .catch(err => {
-                console.error(`Ошибка при попытке включить полноэкранный режим: ${err.message}`);
-                checkbox.checked = false; // Сбрасываем чекбокс в случае ошибки
-            });
+        document.documentElement.requestFullscreen() // Включаем полноэкранный режим
+        .catch(err => {
+            console.error(`Ошибка при попытке включить полноэкранный режим: ${err.message}`);
+            checkbox.checked = false; // Сбрасываем чекбокс в случае ошибки
+        });
     } else {
-        // Выходим из полноэкранного режима
-        document.exitFullscreen()
-            .catch(err => {
-                console.error(`Ошибка при попытке выйти из полноэкранного режима: ${err.message}`);
-            });
+        document.exitFullscreen()  // Выходим из полноэкранного режима
+        .catch(err => { console.error(`Ошибка при попытке выйти из полноэкранного режима: ${err.message}`);});
     }
 });
 
@@ -54,150 +40,120 @@ checkbox.addEventListener('change', () => {
 const window_settings = document.getElementById('window_settings');
 const window_game = document.getElementById('window_game');
 const window_result = document.getElementById('window_result');
-const container = document.getElementById('container');
 const towindow_gameButton = document.getElementById('towindow_game');
 const backTowindow_settingsButton = document.getElementById('backTowindow_settings');
 
+
 // Функция для скрытия всех окон
 function hideAllWindows() {
-    window_settings.classList.remove('active');
-    window_game.classList.remove('active');
-    window_result.classList.remove('active');
+    window_settings.style.display = 'none';
+    window_game.style.display = 'none';
+    window_result.style.display = 'none';
 }
 
 // Переход к окну 2
 towindow_gameButton.addEventListener('click', () => {
     hideAllWindows(); // Скрываем все окна
-    
     overlay.style.display = 'flex'; // Показываем overlay
-        // Скрываем через 5 секунд
-        setTimeout(() => {
-            overlay.style.display = 'none';
-            window_game.classList.add('active'); // Показываем окно 2
-            requestWakeLock();
-	isWindowActive = true;
-//Старт игры
-		
-            startGame();
-
-        }, 2000);
-   
- //   alert(`Контрольная точка 1`);
-
-    loadBestTime(); // Загружаем лучшее время для выбранного размера
+    setTimeout(() => {
+        overlay.style.display = 'none';
+        window_game.classList.add('active'); // Показываем окно 2
+        requestWakeLock();
+	    isWindowActive = true;
+        startGame(); //Старт игры
+    }, 1000);
+    loadBestScore(); // Загружаем лучший результат
 });
 
 // Переход обратно к окну 1
 backTowindow_settingsButton.addEventListener('click', () => {
     hideAllWindows(); // Скрываем все окна
     isWindowActive = false;
-    window_settings.classList.add('active'); // Показываем окно 1
+    window_settings.style.display = 'block'; // Показываем окно настроек
 });
 
 //ИГРА
 
+let bestScore = null;
+const scoreElement = document.getElementById('score');
+const bestScoreElement = document.getElementById('best-score');
+const message = document.getElementById('message');
 
-let gridSize = 5; // Начальный размер таблицы
+let score = 0; // Переменная для хранения текущего счета
+let count = 10;
+let timeLimit = 10;
+let time_out;
 
-let startTime;
-let timerInterval;
-let elapsed = 0; // Хранит общее время
-let bestTime = null;
-
-const gridElement = document.getElementById('grid');
-const timeElement = document.getElementById('time');
-const bestTimeElement = document.getElementById('best-time');
-const gridSizeElement = document.getElementById('grid-size');
-
-
-
-// Старт игры
-
-let timeLimit = 5;
-
-function startGame() {
-    window_game.classList.add('active');
-    setTimeout(() => {
-        alert(`Контрольная точка 2`);
-        endGame(); // Если все числа найдены
-    }, timeLimit*1000 + 500);
-
-    startTime = new Date();
-    timerInterval = setInterval(updateTimer, 100); // Обновляем таймер каждые 100мс
-
-    startProgressBar(timeLimit * 1000); // Передаем время для синхронизации 
-    
-    const interval = setInterval(() => {
-   
-        
-        updateTimer();
-    }, 10);
-    
-}    
-// Обновление таймера
-function updateTimer() {
-    if (!timerInterval) return;
-    const currentTime = new Date();
-    const elapsed = (currentTime - startTime) / 1000;
-    timeElement.textContent = elapsed.toFixed(1);
+function updateWordCount(value) {
+    document.querySelector('label[for="count"]').textContent = `Количество (${value}):`;
+}
+function updateTimeLimit(value) {
+    document.querySelector('label[for="timeLimit"]').textContent = `Время (${value} сек):`;
 }
 
+
+
+const progressBar = document.getElementById('progress');
+function startGame() {
+    score = 0;
+    count = document.getElementById('count').value;
+    timeLimit = document.getElementById('timeLimit').value;
+    loadImages();
+    window_game.style.display = 'block';
+    startProgressBar(timeLimit * 1000); // Передаем время для синхронизации 
+    time_out = setTimeout(() => {
+      //  alert(`Время истекло!`);
+        endGame(); // Если время истекло    
+    },  + timeLimit*1000);
+}    
+
 function startProgressBar(duration) {
-    const progressBar = document.getElementById('progress');
-    
-    // Сбрасываем ширину и убираем transition, чтобы не было мгновенного заполнения
-    progressBar.style.width = '0%';
-    progressBar.style.transition = 'none'; 
-    
-    // Запускаем плавное увеличение ширины после небольшого тайм-аута, чтобы сброс применился
-    setTimeout(() => {
+    progressBar.style.width = '0%'; // Сбрасываем ширину
+    progressBar.style.transition = 'none';// и убираем transition, чтобы не было мгновенного заполнения
+    setTimeout(() => { // Запускаем плавное увеличение ширины после небольшого тайм-аута, чтобы сброс применился
         progressBar.style.transition = `width ${duration}ms linear`; // Устанавливаем плавную анимацию
         progressBar.style.width = '100%'; // Увеличиваем до 100% за заданное время
-    }, 50);  // Небольшая задержка, чтобы CSS успел сбросить ширину
+     }, 50);  // Небольшая задержка, чтобы CSS успел сбросить ширину
 }
 
 // Окончание игры
+const bestScoreKey = `bestScore`;
+
 function endGame() {
-    const time_Elasted = document.getElementById('time_elasted');
+    const gridElement = document.getElementById("grid");
+    gridElement.innerHTML = '';
+    clearTimeout(time_out);
     hideAllWindows(); // Скрываем все окна
-    window_result.classList.add('active'); // Показываем окно 3
-
-    clearInterval(timerInterval); // Останавливаем таймер
-    timerInterval = null; // Обнуляем переменную, чтобы не обновлять таймер дальше
-   
-    elapsed = 0;
-    const currentTime = parseFloat(timeElement.textContent);
-    const bestTimeKey = `bestTime_${gridSize}x${gridSize}`;
-    time_Elasted.textContent = timeElement.textContent + " секунд";
-    // Проверяем, есть ли лучшее время в localStorage
-    const storedBestTime = localStorage.getItem(bestTimeKey);
-    if (!storedBestTime || currentTime < parseFloat(storedBestTime)) {
-        localStorage.setItem(bestTimeKey, currentTime);
-        bestTime = currentTime;
-      //  alert(`Новый рекорд! Вы нашли все числа за ${currentTime.toFixed(1)} секунд.`);
-    } else {
-      //  alert(`Игра окончена! Вы нашли все числа за ${currentTime.toFixed(1)} секунд.`);
-    }
-
-    loadBestTime(); // Загружаем лучшее время
+    window_result.style.display = 'block'; // Показываем окно 3
+    scoreElement.textContent = `${score}`;
+    const storedBestScore = localStorage.getItem(bestScoreKey);
+    if (!storedBestScore || score > parseFloat(storedBestScore)) {
+        localStorage.setItem(bestScoreKey, score);
+    } 
+    loadBestScore(); // Загружаем лучшее время
 }
 
 // Загрузка лучшего времени из localStorage
-function loadBestTime() {
-    const bestTimeKey = `bestTime_${gridSize}x${gridSize}`;
-    const storedBestTime = localStorage.getItem(bestTimeKey);
-
-    if (storedBestTime) {
-        bestTime = parseFloat(storedBestTime);
-        bestTimeElement.textContent = `${bestTime.toFixed(1)} секунд`;
+function loadBestScore() {
+    const storedBestScore = localStorage.getItem(bestScoreKey);
+    if (storedBestScore) {
+        bestScore = parseFloat(storedBestScore);
+        bestScoreElement.textContent = `${bestScore}`;
     } else {
-        bestTimeElement.textContent = '—';
+        bestScoreElement.textContent = '—';
     }
 }
 
 
 // Запускаем игру при загрузке страницы
 window.onload = () => {
+    count = document.getElementById('count').value;
+    timeLimit = document.getElementById('timeLimit').value;  
+    updateWordCount(count); // Обновляем текст ползунка количества слов с текущим значением
+    updateTimeLimit(timeLimit);   // Обновляем текст ползунка времени с текущим значением
+    window_game.style.display = 'none';
+    window_result.style.display = 'none';
+    
     document.body.style.transformOrigin = 'top center';
     document.body.style.transform = 'scale(1)';
     document.body.style.backgroundSize = 'auto';
@@ -205,21 +161,41 @@ window.onload = () => {
         document.body.style.transform = 'scale(' + document.documentElement.clientWidth/500 + ')';
         document.body.style.backgroundSize = '100% auto';
     }
-    loadBestTime(); // Загружаем лучшее время при загрузке страницы
+    
+  //  loadBestScore(); // Загружаем лучшее время при загрузке страницы
 };
 
-
-function updateWindowSize(){
-    const width = window.innerWidth;
-  
-    document.body.style.transform = 'scale(1)';
-    document.body.style.backgroundSize = 'auto';
-    if (width < 500) {
-        //document.body.style.transform = 'scale(' + width/500 + ')';
-        document.body.style.backgroundSize = '100% auto';
-        document.body.style.transform = 'scale(0.75)';
+function getRandomUniqueNumbers(n, max) {
+    const numbers = new Set();
+    while (numbers.size < n) {
+        const random = Math.floor(Math.random() * max) + 1;
+        numbers.add(random);
     }
-    
+    return Array.from(numbers);
 }
 
-//window.addEventListener('resize', updateWindowSize);
+// Функция загрузки и отображения картинок
+function loadImages() {
+    
+    const grid = document.getElementById("grid");
+    const randomNumbers = getRandomUniqueNumbers(count, 100);
+
+    randomNumbers.forEach(num => {
+        const imgContainer = document.createElement("div");
+        imgContainer.className = "image-container";
+
+        const img = document.createElement("img");
+        img.src = `img/${num}.png`;
+        img.alt = `${num}.png`;
+
+        // Добавляем обработчик клика на картинку
+        img.addEventListener("click", () => {
+            score++;
+            message.innerHTML = `Нажато <label style="color: white;">${img.alt}</label>, всего ${score}`;
+            //alert(`You clicked: ${img.alt}`);
+        });
+
+        imgContainer.appendChild(img);
+        grid.appendChild(imgContainer);
+    });
+}
